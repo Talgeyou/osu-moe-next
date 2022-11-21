524361abc2b9c95a456533b56415a1ae5539c8d7
talge <talgeyou@gmail.com>
2022-11-22 07:44:22 +1000

    feat: more meta

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,11 @@ export default function Home() {
                 <meta
                     name="description"
                     content="osu tools for checking statistics of players and comparing them"
-                ></meta>
+                />
+                <meta
+                    name="google-site-verification"
+                    content="Eh4weCPQYX0-vf3VOvB3_R3sz0bU-0CX08s80w7vFPo"
+                />
             </Head>
             <h1>Home page</h1>
         </>
